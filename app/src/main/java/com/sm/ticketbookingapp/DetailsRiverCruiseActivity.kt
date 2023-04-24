a9660f1f92f4ada019eae0950c02226277798fd4
filelucker <filelucker@gmail.com>
2023-04-24 21:03:34 +0600

    shurjopay added

--- app/src/main/java/com/sm/ticketbookingapp/DetailsRiverCruiseActivity.kt
+++ app/src/main/java/com/sm/ticketbookingapp/DetailsRiverCruiseActivity.kt
@@ -4,19 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
+import bd.com.shurjomukhi.v2.model.PaymentReq
+import bd.com.shurjomukhi.v2.model.ShurjopayConfigs
+import bd.com.shurjomukhi.v2.model.ShurjopayException
+import bd.com.shurjomukhi.v2.model.ShurjopaySuccess
+import bd.com.shurjomukhi.v2.payment.PaymentResultListener
+import bd.com.shurjomukhi.v2.payment.Shurjopay
+import com.shurjopay.android.example.utils.Constants
 import com.sm.ticketbookingapp.databinding.ActivityDetailsRiverCruiseBinding
+import java.util.*
 
 class DetailsRiverCruiseActivity : AppCompatActivity() {
     private lateinit var binding:ActivityDetailsRiverCruiseBinding
     private var title = ""
     private var duration = ""
     private var price = ""
+    var shurjopay: Shurjopay? = null
+    private val TAG = "DetailsRiverCruiseActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsRiverCruiseBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        shurjopay = Shurjopay(
+            ShurjopayConfigs(
+                username = Constants.SP_USER,
+                password = Constants.SP_PASS,
+                baseUrl = Constants.SHURJOPAY_API
+            )
+        )
+
 
         title = intent?.getStringExtra("title").toString()
         duration = intent?.getStringExtra("duration").toString()
@@ -66,6 +87,52 @@ class DetailsRiverCruiseActivity : AppCompatActivity() {
                 // Showing the popup menu
                 popupMenu.show()
             }
+            binding.button.setOnClickListener {
+                pay()
+            }
         }
+
+    }
+    private fun pay() {
+
+        val data = PaymentReq(
+            "sp",
+            10.00,
+            "NOK" + Random().nextInt(1000000),
+            "BDT",
+            "moniruzzaman",
+            "mohakhali",
+            "01721915013",
+            "dhaka",
+            "1200",
+            "m.zaman000@gmail.com",
+        )
+
+
+        shurjopay?.makePayment(
+            this,
+            data,
+            object : PaymentResultListener {
+                override fun onSuccess(success: ShurjopaySuccess) {
+                    Log.d(TAG, "onSuccess: debugMessage = ${success.debugMessage}")
+                    Toast.makeText(this@DetailsRiverCruiseActivity, "SUCCESS", Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onFailed(exception: ShurjopayException) {
+                    Log.d(TAG, "onFailed: debugMessage = ${exception.debugMessage}")
+                    Toast.makeText(this@DetailsRiverCruiseActivity, "onFailed: debugMessage = ${exception.debugMessage}", Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onBackButtonListener(exception: ShurjopayException): Boolean {
+                    Log.d(TAG, "onBackButton: debugMessage = ${exception.debugMessage}")
+                    Toast.makeText(this@DetailsRiverCruiseActivity, "onBackButton: debugMessage = ${exception.debugMessage}", Toast.LENGTH_SHORT).show()
+
+                    return true
+                }
+
+            })
+
     }
 }
