6f8c8bc117d5356fccd5687b9a9a128bab8d100d
Abu Zafar Newton <newton@shurjomukhi.com.bd>
2023-04-25 11:42:36 +0600

    All functionality done.

--- app/src/main/java/com/sm/ticketbookingapp/DetailsRiverCruiseActivity.kt
+++ app/src/main/java/com/sm/ticketbookingapp/DetailsRiverCruiseActivity.kt
@@ -17,6 +17,7 @@ import bd.com.shurjomukhi.v2.payment.PaymentResultListener
 import bd.com.shurjomukhi.v2.payment.Shurjopay
 import com.sm.ticketbookingapp.databinding.ActivityDetailsRiverCruiseBinding
 import com.sm.ticketbookingapp.databinding.CongratulationsDialogBinding
+import com.sm.ticketbookingapp.databinding.InfoDialogBinding
 import com.sm.ticketbookingapp.util.Util.Companion.SP_BASE_URL
 import com.sm.ticketbookingapp.util.Util.Companion.SP_PASSWORD
 import com.sm.ticketbookingapp.util.Util.Companion.SP_USER
@@ -27,6 +28,8 @@ class DetailsRiverCruiseActivity : AppCompatActivity() {
     private var title = ""
     private var duration = ""
     private var price = ""
+    private var partial = false
+    private var partialAmount = 0
 
     var shurjopay: Shurjopay? = null
 
@@ -53,20 +56,36 @@ class DetailsRiverCruiseActivity : AppCompatActivity() {
                 Toast.makeText(this,"Please select at least one person", Toast.LENGTH_LONG).show()
             }else{
                 val payo = binding.tvPrice.text.toString().split(" ")
+                var data:PaymentReq? = null
+                if(partial){
+                    partial = false
+                    data = PaymentReq(
+                        "sp",
+                        partialAmount.toDouble(),
+                        "NOK" + Random().nextInt(1000000),
+                        "BDT",
+                        "Abu Zafar Newton",
+                        customerAddress = "shurjoMukhi HQ",
+                        customerPhone = "01766767677",
+                        customerCity = "Dhaka",
+                        "1200",
+                        "msiazn@gmail.com",
+                    )
+                }else{
+                    data = PaymentReq(
+                        "sp",
+                        payo[0].toDouble(),
+                        "NOK" + Random().nextInt(1000000),
+                        "BDT",
+                        "Abu Zafar Newton",
+                        customerAddress = "shurjoMukhi HQ",
+                        customerPhone = "01766767677",
+                        customerCity = "Dhaka",
+                        "1200",
+                        "msiazn@gmail.com",
+                    )
+                }
 
-
-                val data = PaymentReq(
-                    "sp",
-                    1.toDouble(),
-                    "NOK" + Random().nextInt(1000000),
-                    "BDT",
-                    "Abu Zafar Newton",
-                    customerAddress = "shurjoMukhi HQ",
-                    customerPhone = "01766767677",
-                    customerCity = "Dhaka",
-                    "1200",
-                    "msiazn@gmail.com",
-                )
 
 
                 shurjopay?.makePayment(
@@ -101,6 +120,19 @@ class DetailsRiverCruiseActivity : AppCompatActivity() {
         binding.apply {
             backButton.setOnClickListener {
                 finish()
+            }
+            button2.setOnClickListener {
+                if(tvPrice.text == "0 BDT" ||tvPrice.text.isEmpty()){
+                    Toast.makeText(this@DetailsRiverCruiseActivity,"Please select at least one person", Toast.LENGTH_LONG).show()
+                }else{
+                    showInfoDialog{
+                        partial = true
+                        val am = tvPrice.text.toString().trim().split(" ")
+                        partialAmount = (am[0].toInt() * 0.3).toInt()
+                        pay()
+                    }
+                }
+
             }
 
             button.setOnClickListener{
@@ -150,6 +182,20 @@ class DetailsRiverCruiseActivity : AppCompatActivity() {
         val dialog = Dialog(this)
         val binding = CongratulationsDialogBinding.inflate(this.layoutInflater)
         binding.tvMessage.text = "Payment for your trip Successfully. You will be notified soon."
+        dialog.setContentView(binding.root)
+        dialog.setCancelable(false)
+        binding.btnOk.setOnClickListener {
+            dialog.dismiss()
+            onOkClick()
+        }
+        dialog.show()
+    }
+
+
+    private fun showInfoDialog(onOkClick: () -> Unit) {
+        val dialog = Dialog(this)
+        val binding = InfoDialogBinding.inflate(this.layoutInflater)
+        binding.tvMessage.text = "Booking involves availability of seats and 30% charge of total fare"
         dialog.setContentView(binding.root)
         dialog.setCancelable(false)
         binding.btnOk.setOnClickListener {
--- app/src/main/java/com/sm/ticketbookingapp/HelicopterActivity.kt
+++ app/src/main/java/com/sm/ticketbookingapp/HelicopterActivity.kt
@@ -11,6 +11,7 @@ import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import bd.com.shurjomukhi.v2.model.PaymentReq
 import bd.com.shurjomukhi.v2.model.ShurjopayConfigs
 import bd.com.shurjomukhi.v2.model.ShurjopayException
@@ -19,6 +20,7 @@ import bd.com.shurjomukhi.v2.payment.PaymentResultListener
 import bd.com.shurjomukhi.v2.payment.Shurjopay
 import com.sm.ticketbookingapp.databinding.ActivityHelicoptorBinding
 import com.sm.ticketbookingapp.databinding.CongratulationsDialogBinding
+import com.sm.ticketbookingapp.databinding.InfoDialogBinding
 import com.sm.ticketbookingapp.util.Util
 import com.sm.ticketbookingapp.util.Util.Companion.HelicopterPricePP
 import java.util.Random
@@ -26,6 +28,8 @@ import java.util.Random
 
 class HelicopterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHelicoptorBinding
+    private var partial = false
+    private var partialAmount = 0
 
     var shurjopay: Shurjopay? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,12 +46,141 @@ class HelicopterActivity : AppCompatActivity() {
         )
 
         binding.apply {
+            button2.setOnClickListener {
+                if(tvPrice.text == "0 BDT" ||tvPrice.text.isEmpty()){
+                    Toast.makeText(this@HelicopterActivity,"Please select at least one person", Toast.LENGTH_LONG).show()
+                }else{
+                    showInfoDialog{
+                        partial = true
+                        val am = tvPrice.text.toString().trim().split(" ")
+                        partialAmount = (am[0].toInt() * 0.3).toInt()
+                        pay()
+                    }
+                }
+
+            }
 
             binding.dr.setOnClickListener {
                 openDatePicker()
             }
             binding.dr0.setOnClickListener {
                 openTimePicker()
+            }
+            binding.dr1.setOnClickListener {
+                val popupMenu = PopupMenu(this@HelicopterActivity, it)
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.menuInflater.inflate(com.sm.ticketbookingapp.R.menu.origin_drop_down, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                    when(menuItem.title){
+                        "Dhaka" -> {
+                            dr1.text = "Dhaka Helipad"
+                        }
+                        "Chattagram" -> {
+                            dr1.text = "Chattagram Helipad"
+                        }
+                        "Cumilla" -> {
+                            dr1.text = "Cumilla Helipad"
+                        }
+                        "Rangpur" -> {
+                            dr1.text = "Rangpur Helipad"
+                        }
+                        "Khulna" -> {
+                            dr1.text = "Khulna Helipad"
+                        }
+                        "Barisal" -> {
+                            dr1.text = "Barisal Helipad"
+                        }
+                        "Sylhet" -> {
+                            dr1.text = "Sylhet Helipad"
+                        }
+                    }
+                    true
+                }
+                // Showing the popup menu
+                popupMenu.show()
+            }
+
+
+            binding.dr2.setOnClickListener {
+                val popupMenu = PopupMenu(this@HelicopterActivity, it)
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.menuInflater.inflate(com.sm.ticketbookingapp.R.menu.origin_drop_down, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                    when(menuItem.title){
+                        "Dhaka" -> {
+                            dr2.text = "Dhaka Helipad"
+                        }
+                        "Chattagram" -> {
+                            dr2.text = "Chattagram Helipad"
+                        }
+                        "Cumilla" -> {
+                            dr2.text = "Cumilla Helipad"
+                        }
+                        "Rangpur" -> {
+                            dr2.text = "Rangpur Helipad"
+                        }
+                        "Khulna" -> {
+                            dr2.text = "Khulna Helipad"
+                        }
+                        "Barisal" -> {
+                            dr2.text = "Barisal Helipad"
+                        }
+                        "Sylhet" -> {
+                            dr2.text = "Sylhet Helipad"
+                        }
+                    }
+                    true
+                }
+                // Showing the popup menu
+                popupMenu.show()
+            }
+
+            binding.dr3.setOnClickListener {
+                val popupMenu = PopupMenu(this@HelicopterActivity, it)
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.menuInflater.inflate(com.sm.ticketbookingapp.R.menu.trip_type, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                    when(menuItem.title){
+                        "One way" -> {
+                            dr3.text = "One way"
+                        }
+                        "Round Trip" -> {
+                            dr3.text = "Round Trip"
+                        }
+
+                    }
+                    true
+                }
+                // Showing the popup menu
+                popupMenu.show()
+            }
+
+            binding.dr5.setOnClickListener {
+                val popupMenu = PopupMenu(this@HelicopterActivity, it)
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.menuInflater.inflate(com.sm.ticketbookingapp.R.menu.yes_no_menu, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                    when(menuItem.title){
+                        "Yes" -> {
+                            dr5.text = "Yes"
+                        }
+                        "No" -> {
+                            dr5.text = "No"
+                        }
+
+                    }
+                    true
+                }
+                // Showing the popup menu
+                popupMenu.show()
             }
 
             button.setOnClickListener {
@@ -86,20 +219,37 @@ class HelicopterActivity : AppCompatActivity() {
 
 
             val payo = binding.tvPrice.text.toString().split(" ")
+             var data:PaymentReq? = null
+            if(partial){
+                partial = false
+                data = PaymentReq(
+                    "sp",
+                    partialAmount.toDouble(),
+                    "NOK" + Random().nextInt(1000000),
+                    "BDT",
+                    "Abu Zafar Newton",
+                    customerAddress = "shurjoMukhi HQ",
+                    customerPhone = "01766767677",
+                    customerCity = "Dhaka",
+                    "1200",
+                    "msiazn@gmail.com",
+                )
+            }else{
+                data = PaymentReq(
+                    "sp",
+                    payo[0].toDouble(),
+                    "NOK" + Random().nextInt(1000000),
+                    "BDT",
+                    "Abu Zafar Newton",
+                    customerAddress = "shurjoMukhi HQ",
+                    customerPhone = "01766767677",
+                    customerCity = "Dhaka",
+                    "1200",
+                    "msiazn@gmail.com",
+                )
+            }
 
 
-            val data = PaymentReq(
-                "sp",
-                1.toDouble(),
-                "NOK" + Random().nextInt(1000000),
-                "BDT",
-                "Abu Zafar Newton",
-                customerAddress = "shurjoMukhi HQ",
-                customerPhone = "01766767677",
-                customerCity = "Dhaka",
-                "1200",
-                "msiazn@gmail.com",
-            )
 
 
             shurjopay?.makePayment(
@@ -166,6 +316,17 @@ class HelicopterActivity : AppCompatActivity() {
         timePickerDialog.show()
     }
 
-
+    private fun showInfoDialog(onOkClick: () -> Unit) {
+        val dialog = Dialog(this)
+        val binding = InfoDialogBinding.inflate(this.layoutInflater)
+        binding.tvMessage.text = "Booking involves availability of seats and 30% charge of total fare"
+        dialog.setContentView(binding.root)
+        dialog.setCancelable(false)
+        binding.btnOk.setOnClickListener {
+            dialog.dismiss()
+            onOkClick()
+        }
+        dialog.show()
+    }
 
 }
