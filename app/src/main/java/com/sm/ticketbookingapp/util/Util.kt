0ccd0d5db92a57cfca1090363812da9183016c53
Abu Zafar Newton <newton@shurjomukhi.com.bd>
2023-04-25 10:31:43 +0600

    credientials changed for sandbox

--- app/src/main/java/com/sm/ticketbookingapp/util/Util.kt
+++ app/src/main/java/com/sm/ticketbookingapp/util/Util.kt
@@ -3,9 +3,9 @@ package com.sm.ticketbookingapp.util
 
 class Util {
     companion object {
-        const val SP_USER = "shurjopay"
-        const val SP_PASSWORD = "pyyk97hu&6u625"
-        const val SP_BASE_URL = "https://stagingapp.engine.shurjopayment.com"
+        const val SP_USER = "sp_sandbox"
+        const val SP_PASSWORD = "pyyk97hu&6u6"
+        const val SP_BASE_URL = "https://www.sandbox.shurjopayment.com"
         const val HelicopterPricePP = 1_00_000
     }
 }
